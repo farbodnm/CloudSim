eee5da619aa44547d479491cf7c151a66048e954
farbod <farb0d.nm99@gmail.com>
2025-01-26 17:50:17 +0330

    Create serverless controller entity

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/scheduling/ServerlessController.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/scheduling/ServerlessController.java
@@ -78,97 +78,18 @@ public class ServerlessController extends ContainerDatacenterBroker {
     }
 
     private void submitRequest(SimEvent ev) {
-        ServerlessRequest request = (ServerlessRequest) ev.getData();
-        log.info("Request: {} arrived at: {}", request.getCloudletId(), CloudSim.clock());
-        if (CloudSim.clock() == requestSubmitClock) {
-            send(
-                    getId(),
-                    Constants.MINIMUM_INTERVAL_BETWEEN_TWO_CLOUDLET_SUBMISSIONS,
-                    CloudSimSCTags.CLOUDLET_SUBMIT,
-                    request
-            );
-        }
-        else {
-            datacenter.updateCloudletProcessing();
-            getCloudletList().add(request);
-            loadBalancer.routeRequest(request);
-        }
+
     }
 
     private void processRequestSubmitAck(SimEvent ev) {
-        ServerlessRequest request = (ServerlessRequest) ev.getData();
-        ServerlessContainer container = ContainerList.getById(getContainersCreatedList(), request.getContainerId());
-        if (container != null) {
-            container.setFirstProcess(false);
-        }
+
     }
 
     private void processContainerDestroy(SimEvent ev) {
-        int[] data = (int[]) ev.getData();
-        // data[0](DC ID) Ignored since we have one dc.
-        int containerId = data[1];
-        int result = data[2];
-        int vmId = data[3];
-        ServerlessContainer container = ContainerList.getById(getContainersCreatedList(), containerId);
-        try {
-            if (Objects.equals(result, CloudSimSCTags.TRUE)) {
-                log.info("Container: {} was destroyed at: {}", containerId, CloudSim.clock());
-                ServerlessInvoker invoker = (ContainerVmList.getById(getVmsCreatedList(), vmId));
-                assert invoker != null;
-                if (invoker.getContainerList().isEmpty()) {
-                    vmIdleList.add(invoker);
-                }
-                getContainersToVmsMap().remove(containerId);
-                getContainersCreatedList().remove(container);
-                containerDestroyedList.add(container);
-                assert container != null;
-                container.setFinishTime(CloudSim.clock());
-                setContainersCreated(getContainersCreated() - 1);
-            } else {
-                throw new RuntimeException("Container failed to be destroyed");
-            }
-        } catch (Exception e) {
-            log.error("Something went wrong with the destruction of container: {} at: {} with message: {}",
-                    containerId,
-                    CloudSim.clock(),
-                    e.getMessage()
-            );
-        }
+
     }
 
     private void recordResourceUtilization(SimEvent ignored) {
 
-        double utilization;
-        int vmCount = 0;
-        double utilizationSum = 0;
-        for (ContainerVm vm: vmsCreatedList) {
-            ServerlessInvoker invoker = (ServerlessInvoker) vm;
-            utilization = 1 - invoker.getAvailableMips() / invoker.getTotalMips();
-            if (utilization > 0) {
-                invoker.setUsed(true);
-                vmCount++;
-                utilizationSum += utilization;
-            }
-        }
-
-        if (utilizationSum > 0) {
-            averageVmUsageRecords.add(utilizationSum / vmCount);
-            vmCountList.add(vmCount);
-        }
-
-        if (averageVmUsageRecords.size() >= Constants.CPU_HISTORY_LENGTH) {
-            double sumOfAverages = 0;
-            double sumOfVmCounts = 0;
-            for (int i = 0; i < averageVmUsageRecords.size(); i++) {
-                sumOfAverages += averageVmUsageRecords.get(i);
-                sumOfVmCounts += vmCountList.get(i);
-            }
-            meanAverageVmUsageRecords.add(sumOfAverages / averageVmUsageRecords.size());
-            meanSumOfVmCounts.add(sumOfVmCounts / vmCountList.size());
-            averageVmUsageRecords.clear();
-            vmCountList.clear();
-        }
-
-        send(this.getId(), Constants.CPU_USAGE_MONITORING_INTERVAL, CloudSimSCTags.RECORD_CPU_USAGE);
     }
 }
