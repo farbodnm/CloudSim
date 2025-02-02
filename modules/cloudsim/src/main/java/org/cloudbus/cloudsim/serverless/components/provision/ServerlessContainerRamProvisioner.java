7208d6f785fc40c3d53b0eb9f8af2d2071700e39
farbod <farb0d.nm99@gmail.com>
2025-02-02 18:57:17 +0330

    Finish serverless ram provisioner

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/provision/ServerlessContainerRamProvisioner.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/provision/ServerlessContainerRamProvisioner.java
@@ -1,6 +1,8 @@
 package org.cloudbus.cloudsim.serverless.components.provision;
 
 import org.cloudbus.cloudsim.container.containerProvisioners.ContainerRamProvisionerSimple;
+import org.cloudbus.cloudsim.container.core.Container;
+import org.cloudbus.cloudsim.serverless.components.process.ServerlessContainer;
 
 public class ServerlessContainerRamProvisioner extends ContainerRamProvisionerSimple {
 
@@ -8,5 +10,18 @@ public class ServerlessContainerRamProvisioner extends ContainerRamProvisionerSi
         super(availableRam);
     }
 
+    @Override
+    public boolean allocateRamForContainer(Container container, float ram) {
 
+        float oldRam = container.getCurrentAllocatedRam();
+        if (getAvailableVmRam() + oldRam >= ram) {
+            deallocateRamForContainer(container);
+            setAvailableVmRam(getAvailableVmRam() - ram);
+            getContainerRamTable().put(container.getUid(), ram);
+            container.setCurrentAllocatedRam(ram);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
