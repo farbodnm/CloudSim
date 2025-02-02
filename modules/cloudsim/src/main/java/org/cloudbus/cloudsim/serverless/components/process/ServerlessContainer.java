dd58c327f22f063a77d00b4c54396a42f82ed555
farbod <farb0d.nm99@gmail.com>
2025-02-02 18:48:57 +0330

    Finish serverless container

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/process/ServerlessContainer.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/process/ServerlessContainer.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.cloudbus.cloudsim.container.core.Container;
 import org.cloudbus.cloudsim.container.schedulers.ContainerCloudletScheduler;
+import org.cloudbus.cloudsim.serverless.components.scheduling.ServerlessRequestScheduler;
 import org.cloudbus.cloudsim.serverless.components.transfer.ServerlessRequest;
 
 import java.util.ArrayList;
@@ -21,8 +22,7 @@ public class ServerlessContainer extends Container {
     private double finishTime = 0;
     private boolean reschedule;
     private boolean idling;
-    // TODO: Maybe use enum?
-    private  String functionType;
+    private String functionType;
 
     private List<ServerlessRequest> pendingTasks = new ArrayList<>();
     private List<ServerlessRequest> runningTasks = new ArrayList<>();
@@ -50,6 +50,9 @@ public class ServerlessContainer extends Container {
     }
 
     public double updateContainerProcessing(double currentTime, List<Double> mipsShare, ServerlessInvoker invoker) {
-        return 0.0D;
+        if (mipsShare != null) {
+            return ((ServerlessRequestScheduler) getContainerCloudletScheduler()).updateContainerProcessing(currentTime, mipsShare, invoker);
+        }
+        return 0D;
     }
 }
