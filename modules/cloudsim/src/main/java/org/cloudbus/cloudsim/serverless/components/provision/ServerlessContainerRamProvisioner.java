1fe82a26e29bfb64674718a3b52a20f3f5c23eae
farbod <farb0d.nm99@gmail.com>
2025-02-15 18:33:17 +0330

    Refactor provision package

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/provision/ServerlessContainerRamProvisioner.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/provision/ServerlessContainerRamProvisioner.java
@@ -2,35 +2,26 @@ package org.cloudbus.cloudsim.serverless.components.provision;
 
 import org.cloudbus.cloudsim.container.containerProvisioners.ContainerRamProvisionerSimple;
 import org.cloudbus.cloudsim.container.core.Container;
+import org.cloudbus.cloudsim.serverless.components.process.ServerlessContainer;
 
 public class ServerlessContainerRamProvisioner extends ContainerRamProvisionerSimple {
+
     public ServerlessContainerRamProvisioner(float availableRam) {
         super(availableRam);
-
     }
 
     @Override
-    public boolean allocateRamForContainer(Container container,
-                                           float ram) {
-        float oldRam = container.getCurrentAllocatedRam();
-        deallocateRamForContainer(container);
+    public boolean allocateRamForContainer(Container container, float ram) {
 
-        if (getAvailableVmRam() >= ram) {
-            System.out.println("New available ram: "+(getAvailableVmRam() - ram));
+        float oldRam = container.getCurrentAllocatedRam();
+        if (getAvailableVmRam() + oldRam >= ram) {
+            deallocateRamForContainer(container);
             setAvailableVmRam(getAvailableVmRam() - ram);
             getContainerRamTable().put(container.getUid(), ram);
             container.setCurrentAllocatedRam(ram);
             return true;
+        } else {
+            return false;
         }
-
-
-
-        container.setCurrentAllocatedRam(oldRam);
-
-        return false;
     }
-
-
-
-
 }
--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/provision/UtilizationModelPartial.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/serverless/components/provision/UtilizationModelPartial.java
@@ -7,20 +7,21 @@ import org.cloudbus.cloudsim.serverless.components.transfer.ServerlessRequest;
  * New utilization model class for partial usage of a resource
  *
  * @author Anupama Mampage
+ * @author Farbod Nazari
  */
 
 public class UtilizationModelPartial implements UtilizationModel {
+
     @Override
     public double getUtilization(double time) {
         return 0;
     }
 
-    public double getCpuUtilization(ServerlessRequest request) {
-        return request.getCpuShareRequest();
+    public double getCPUUtilization(ServerlessRequest request) {
+        return request.getRequestCPUShare();
     }
+
     public double getMemUtilization(ServerlessRequest request) {
-        return request.getMemShareRequest();
+        return request.getRequestMemShare();
     }
-
-
 }
